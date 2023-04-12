b040e4d35dde19482f2e6593c09059acbd5f5448
sgustjd2 <shin1133@naver.com>
2023-04-12 09:35:33 +0900

    git ignore web부분 삭제 후 재 commit

---
+++ clientweb/src/main/webapp/common/js/exam.js
@@ -0,0 +1,9 @@
+function test1(){
+    alert("alert 실행");
+}
+function test2(){
+    confirm("confirm 실행");
+}
+function test3(){
+    prompt("promt 실행");
+}
---
+++ clientweb/src/main/webapp/common/js/mytest.js
@@ -0,0 +1,7 @@
+/**
+ * 
+ 외부 파일에서 mytest.js 선언
+ */
+ function test2(){
+	alert("외부 문서에 정의된 함수")
+}
